7c27c8b54b35cb19b9fb919dc20343eb9912c9b5
Andor Saga <andor@idearebel.com>
2017-10-13 15:57:22 -0400

    fixed watch (script wasnt in a head tag.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,177 +2,193 @@
 
 module.exports = function(grunt) {
 
-    const LivereloadPort = 35729;
-    const ServeStatic = require('serve-static');
+  const LivereloadPort = 35729;
+  const ServeStatic = require('serve-static');
 
-    let page = grunt.option('page') || 'src/index.html';
-    const dist = 'dist';
-    const src = 'src';
-    const tmp = '.tmp';
-    const app = `app`;
+  let page = grunt.option('page') || 'src/index.html';
+  const dist = 'dist';
+  const src = 'src';
+  const tmp = '.tmp';
+  const app = `app`;
 
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  // const LivereloadPort = 35729;
+  // const ServeStatic = require('serve-static');
 
-    grunt.initConfig({
-        project: {
-            src: 'src'
-        },
+  
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-        /** 
-         * Delete all working directories and contents
-         */
-        clean: {
-            build: {
-                src: [
-                    `${app}`,
-                    `${dist}`,
-                    `${tmp}`
-                ]
-            }
-        },
+  grunt.initConfig({
+    project: {
+      src: 'src'
+    },
 
-
-        /**
-         * https://github.com/jsoverson/grunt-open
-         *
-         * Opens the web server in the browser
-         */
-        open: {
-            server: {
-                path: `http://localhost:<%= connect.options.port %>/`
-            }
-        },
+    /** 
+     * Delete all working directories and contents
+     */
+    clean: {
+      build: {
+        src: [
+          `${app}`,
+          `${dist}`,
+          `${tmp}`
+        ]
+      }
+    },
 
 
-        /**
-         * Connect port/livereload
-         * https://github.com/gruntjs/grunt-contrib-connect
-         */
-        connect: {
-            options: {
-                port: 9000,
-                hostname: '*'
-            },
-            livereload: {
-                options: {
-                    middleware: function(connect, options) {
-                        return [
-                            ServeStatic(`${app}`),
-                            connect().use(`${app}`, ServeStatic(`${app}`)),
-                            ServeStatic(`${app}`)
-                        ]
-                    }
-                }
-            }
-        },
-
-        /**
-         *
-         */
-        copy: {
-            dev: {
-                files: [
-                    // MARKUP
-                    {
-                        expand: true,
-                        cwd: `${src}/`,
-                        src: 'index.html',
-                        dest: `${app}/`,
-                        filter: 'isFile'
-                    },
-                    // STYLE
-                    {
-                        expand: true,
-                        cwd: `${src}/css/`,
-                        src: '*.css',
-                        dest: `${app}/`,
-                        filter: 'isFile'
-                    },
-                    // JS
-                    {
-                        expand: true,
-                        cwd: `${src}/js`,
-                        src: '**/*.js',
-                        dest: `${app}/`,
-                        filter: 'isFile'
-                    },
-                    // IMAGES
-                    {
-                        expand: true,
-                        flatten: true,
-                        cwd: `/raw-images/`,
-                        //'<%= project.imgLocal %>/',
-                        src: ['**/*.{jpg,jpeg,png,gif,svg}'],
-                        dest: `${app}/medias/`,
-                        filter: 'isFile'
-                    }
-                ]
-            }           
-        },
+    /**
+     * https://github.com/jsoverson/grunt-open
+     *
+     * Opens the web server in the browser
+     */
+    open: {
+      server: {
+        path: `http://localhost:<%= connect.options.port %>/`
+      }
+    },
 
 
-        /**
-         * https://github.com/gruntjs/grunt-contrib-jshint
-         * options inside .jshintrc file
-         */
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-
-            files: [
-                `${src}/js/**/*.js`,
-                `${src}/json/**/*.js`,
-                `!${src}/js/vendor/**/*.js`
+    /**
+     * Connect port/livereload
+     * https://github.com/gruntjs/grunt-contrib-connect
+     */
+    connect: {
+      options: {
+        port: 9000,
+        hostname: '*'
+      },
+      livereload: {
+        options: {
+          middleware: function(connect, options) {
+            return [
+              ServeStatic(`${app}`),
+              connect().use(`${app}`, ServeStatic(`${app}`)),
+              ServeStatic(`${app}`)
             ]
-        },
-
-
-        /**
-         * https://github.com/gruntjs/grunt-contrib-watch
-         *
-         */
-        watch: {
-            options: {
-                spawn: false,
-                livereload: true
-            },
-            scripts_dev: {
-                 files: [
-                   `${src}/js/*.js`
-                 ],
-                tasks: [
-                 'copy:dev'
-                ],
-                options: {
-                  livereload: true
-                }
-            },
-            markup: {
-                files: [
-                    `src/index.html`
-                ],
-                tasks: [
-                    // 'htmlhint:dev'
-                ],
-                options: {
-                    livereload: true
-                }
-            }
+          }
         }
-    });
+      }
+    },
+
+    /**
+     *
+     */
+    copy: {
+      dev: {
+        files: [
+          // MARKUP
+          {
+            expand: true,
+            cwd: `${src}/`,
+            src: 'index.html',
+            dest: `${app}/`,
+            filter: 'isFile'
+          },
+          // STYLE
+          {
+            expand: true,
+            cwd: `${src}/css/`,
+            src: '*.css',
+            dest: `${app}/`,
+            filter: 'isFile'
+          },
+          // JS
+          {
+            expand: true,
+            cwd: `${src}/js`,
+            src: '**/*.js',
+            dest: `${app}/`,
+            filter: 'isFile'
+          },
+          // IMAGES
+          {
+            expand: true,
+            flatten: true,
+            cwd: `/raw-images/`,
+            //'<%= project.imgLocal %>/',
+            src: ['**/*.{jpg,jpeg,png,gif,svg}'],
+            dest: `${app}/medias/`,
+            filter: 'isFile'
+          }
+        ]
+      }
+    },
 
 
-    grunt.registerTask('default', [
-        // JS
-        // 'jshint',
-        //'browserify:dev',
-        //'uglify:dev',
+    /**
+     * https://github.com/gruntjs/grunt-contrib-jshint
+     * options inside .jshintrc file
+     */
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
 
-        // LIVE UPDATES / PREVIEW
-        'copy:dev',
-        'connect:livereload',
-        'open',
-        'watch'
-    ]);
+      files: [
+        `${src}/js/**/*.js`,
+        `${src}/json/**/*.js`,
+        `!${src}/js/vendor/**/*.js`
+      ]
+    },
+
+
+    /**
+     * https://github.com/gruntjs/grunt-contrib-watch
+     *
+     */
+    watch: {
+      options: {
+        spawn: false,
+        livereload: true
+      },
+      scripts_dev: {
+        files: [
+          `${src}/js/*.js`
+        ],
+        tasks: [
+          'copy:dev'
+        ],
+        options: {
+          livereload: true
+        }
+      },
+      style: {
+        files: [
+          `src/css/style.css`
+        ],
+        tasks: [
+          'copy:dev'
+        ],
+        options: {
+          livereload: true
+        }
+      },
+      markup: {
+        files: [
+          `src/index.html`
+        ],
+        tasks: [
+            'copy:dev'
+          //'htmlhint:dev'
+        ],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+
+  grunt.registerTask('default', [
+    // JS
+    // 'jshint',
+    //'browserify:dev',
+    //'uglify:dev',
+
+    // LIVE UPDATES / PREVIEW
+    'copy:dev',
+    'connect:livereload',
+    'open',
+    'watch'
+  ]);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,6 @@ function setup() {
 
 function draw() {
   background(0);
+  translate(width / 2, height / 2);
+  ellipse(0, 0, 50, 50);
 }
