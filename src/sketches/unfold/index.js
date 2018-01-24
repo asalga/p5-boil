c7c746ba109a92e997a94820bf0daa6c6cac2dc2
Andor Saga <andor.saga@gmail.com>
2018-01-24 10:31:58 -0500

    fixed lines not getting connected.

--- src/sketches/unfold/index.js
+++ src/sketches/unfold/index.js
@@ -18,23 +18,17 @@
  * Easing Functions - inspired from http://gizma.com/easing/
  * only considering the t value for the range [0, 1] => [0, 1]
  */
- // aler('asdf');
 
-
-// import EasingFunctions from '/easing.js';
-// let east = require('./easing.js');
-
-let multiUnfold = false;
 let grid = [];
-let xLen = 10;
-let yLen = 7;
+let xLen = 40;
+let yLen = 20;
 
-let intensityScale = 170;
-let next = 0;
-let xStart = 200;
+let noiseScale = 140;
+let noiseIndex = 0;
+
+let posStart = 800;
 let pos = 0;
-let speed = 0.25 * 5;
-let unfoldSpeed = 1.0;
+let speed = 0.25 * 6;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -47,61 +41,47 @@ function setup() {
       grid[x].push(getNoise(x, y));
     }
   }
+  noFill();
+  stroke(0, 255, 0);
 }
 
 function draw() {
   background(0);
 
   rotateX(1);
-  translate(pos - xStart, 20, 20);
-  // rotateX(mouseX / windowWidth * TWO_PI * 4);
+  translate(pos - posStart, -200, 10);
+  // var rt = mouseX / windowWidth * TWO_PI * 4;
+  rotateX(6.2);
 
   let sc = 30;
-  // beginShape(LINES);
+
   //
   for (let y = 0; y < yLen; ++y) {
-
+    beginShape(LINES);
     // -1 since we need to 'look forward' 1 index
     // -1 since we need to draw the animated line.
     for (let x = 0; x < xLen - 2; ++x) {
       let f = (x < xLen - 1) ? 255 : 40;
       stroke(0, f, 0);
-      line(
-        (x + 0) * sc, y * sc, grid[x][y] * intensityScale,
-      (x + 1) * sc, y * sc, grid[x + 1][y] * intensityScale);
-
-      // vertex((x + 0) * sc, y * sc, grid[x][y] * intensityScale);
-      // vertex((x + 1) * sc, y * sc, grid[x + 1][y] * intensityScale);
+      vertex((x + 0) * sc, y * sc, grid[x][y] * noiseScale);
+      vertex((x + 1) * sc, y * sc, grid[x + 1][y] * noiseScale);
     }
+    endShape();
   }
 
-  // vertex((xLen-2) * sc, yLen-1 * sc, grid[xLen-1][yLen-1] * intensityScale);
-  // endShape();
 
-  // vertex(57.5, 50,100);
-  // vertex(57.5, 15);
-  // vertex(92, 50);
-  // vertex(57.5, 85);
-  // vertex(22, 50);
-  // vertex(57.5, 15);
-
-
-  let calls = 0;
   // towards user
   for (let x = 0; x < xLen - 1; ++x) {
-    // stroke(255, 0, 0);
+    beginShape(LINES);
     for (let y = 0; y < yLen - 1; ++y) {
-      calls++;
-      // stroke(255, 0, 0);
-      line(
-        x * sc, (y + 0) * sc, grid[x][y] * intensityScale,
-        x * sc, (y + 1) * sc, grid[x][y + 1] * intensityScale
-      );
+      vertex(x * sc, (y + 0) * sc, grid[x][y] * noiseScale);
+      vertex(x * sc, (y + 1) * sc, grid[x][y + 1] * noiseScale);
     }
+    endShape();
   }
-  // console.log(calls, frameRate());
 
-  // draw animated lines
+  
+  // Animated lines
   for (let y = 0; y < yLen; ++y) {
     push();
 
@@ -109,30 +89,26 @@ function draw() {
     let startVector = new p5.Vector(
       (xLen - 3) * sc - (xLen - 2) * sc,
       0,
-      grid[xLen - 3][y] * intensityScale - grid[xLen - 2][y] * intensityScale
+      grid[xLen - 3][y] * noiseScale - grid[xLen - 2][y] * noiseScale
     );
     let endVector = new p5.Vector(
       (xLen - 1) * sc - (xLen - 2) * sc,
       0,
-      grid[xLen - 1][y] * intensityScale - grid[xLen - 2][y] * intensityScale);
+      grid[xLen - 1][y] * noiseScale - grid[xLen - 2][y] * noiseScale);
 
-
+    //
     var crossProduct = p5.Vector.cross(startVector, endVector);
 
     let theta = endVector.angleBetween(startVector);
 
     // start the end point at the edge
-    translate((xLen - 2) * sc, y * sc, grid[xLen - 2][y] * intensityScale);
+    translate((xLen - 2) * sc, y * sc, grid[xLen - 2][y] * noiseScale);
 
-    let r = min(1,abs(pos / 30));
-    
-    var e = EasingFunctions.easeOutQuint(r);
-
+    let r = min(1, abs(pos / 20));
+    var e = EasingFunctions.easeInOutQuad(r);
     let dir = crossProduct.y < 0 ? 1 : -1;
     rotateY(theta * -e * dir);
-
     // rotateY(mouseX / windowWidth * TWO_PI * 4);
-
     line(0, 0, 0, startVector.x, startVector.y, startVector.z);
 
     pop();
@@ -141,7 +117,7 @@ function draw() {
   pos -= speed;
   if (pos % 30 === 0) {
     pos = 0;
-    next++;
+    noiseIndex++;
     shiftValues();
   }
 }
@@ -165,6 +141,6 @@ function shiftValues() {
 
   // create new values
   for (let y = 0; y < yLen; ++y) {
-    grid[xLen - 1][y] = getNoise(xLen + next, y);
+    grid[xLen - 1][y] = getNoise(xLen + noiseIndex, y);
   }
 }
