cbe07710ef1bf1af049d7baa5227ae5d8adfd822
Andor Saga <andor.saga@gmail.com>
2018-02-28 17:24:59 -0500

    .

--- src/sketches/robotic-access/index.js
+++ src/sketches/robotic-access/index.js
@@ -1,9 +1,3 @@
-/*
-  Andor Saga
-  Feb 2018
-*/
-
-let looping = true;
 const MaxSize = 75;
 
 function setup() {
@@ -13,17 +7,20 @@ function setup() {
 }
 
 function drawBox(s, c) {
-  fill(255);
+  let n = max(0, s - 50 / 2);
 
+  fill(255);
   if (s > MaxSize - 10) {
-    let col = MaxSize - s;
-    fill(255 * (col / 10));
+    fill(255 * ((MaxSize - s) / 10));
+  }
+  rect(0, 0, s + 2, s + 2);
+
+  if (s > 10) {
+    fill(0);
+    rect(0, 0, s - 10, s - 10);
   }
 
-  rect(0, 0, s, s);
   fill(0);
-  let n = max(0, s - 50 / 2);
-  rect(0, 0, (s / 2) + (n / 3), s / 2 + n / 3);
   rect(0, 0, n / 2, windowHeight);
   rect(0, 0, windowWidth, n / 2);
 }
@@ -31,13 +28,10 @@ function drawBox(s, c) {
 function draw() {
   background(0);
   translate(width / 2, height / 2);
-  scale(3,3);
-  
-  let f = frameCount/4;
-  let arr = [(f + 0) % MaxSize,
-    (f + 25) % MaxSize,
-    (f + 50) % MaxSize
-  ];
+  scale(5, 5);
+
+  let f = frameCount / 4;
+  let arr = [(f + 0) % MaxSize, (f + 25) % MaxSize, (f + 50) % MaxSize];
   arr = sort(arr).reverse();
-  arr.forEach((v, i, a) => drawBox(v));
+  arr.forEach(v => drawBox(v));
 }
