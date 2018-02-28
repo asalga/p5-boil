97fc3c07c4d11cc97e195df2d3b26538c0092602
Andor Saga <andor.saga@gmail.com>
2018-02-28 13:52:31 -0500

    .

--- src/sketches/robotic-access/index.js
+++ src/sketches/robotic-access/index.js
@@ -6,40 +6,27 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noStroke();
-  stroke(255);
   noFill(255);
   rectMode(CENTER);
 }
 
-// 0 - width
-// mod(width)
-// 
-
 function drawBox(s) {
-  let sz = 50; // (1 + (s % 2)) * 100;
-
-  push();
-  //scale(1 + s % 3);
+  let sz = 50;
   rect(0, 0, s, s);
-
-  // fill(0);
-  // rect(0, 0, 130, 130);
-  // rect(0, 0, 50, windowHeight);
-  // rect(0, 0, windowWidth, 50);
-  pop();
 }
 
 function draw() {
   background(0);
+
   translate(width / 2, height / 2);
+  scale(2, 2);
 
-  let fc = frameCount;
+  let f = frameCount / 30;
+  let maxSize = 75;
 
-  //  scale(1 + (frameCount / 100) % 2);
-  let s1 = (fc / 2) % 100;
-  let s2 = ((fc + 50) / 2) % 150;
-  let s3 = ((fc + 100) / 2) % 200;
+  let s1 = (f + 0) % maxSize;
+  let s2 = (f + 25) % maxSize;
+  let s3 = (f + 50) % maxSize;
 
   stroke(0, 255, 0);
   drawBox(s1);
@@ -47,9 +34,6 @@ function draw() {
   stroke(255, 0, 0);
   drawBox(s2);
 
-  stroke(255, 0, 0);
+  stroke(0, 0, 255);
   drawBox(s3);
-
-  // drawBox(s * 2);
-  // drawBox(s * 3);
 }
