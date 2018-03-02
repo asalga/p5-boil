7f0530843ac381fd5e4589505c41c003016a5fdd
Andor Saga <andor.saga@gmail.com>
2018-03-02 13:28:40 -0500

    .

---
+++ src/sketches/_op_frags_0/index.js
@@ -0,0 +1,12 @@
+let sh;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  sh = new p5.Shader(this._renderer, vert, frag);
+}
+
+function draw() {
+  translate(-width / 2, -height / 2);
+  shader(sh);
+  rect(0, 0, windowWidth, windowHeight, 1, 1);
+}
