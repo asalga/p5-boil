c39642d98f4ecb475412be4cdbb53968afb4e4b8
Andor Saga <andor.saga@gmail.com>
2018-03-03 13:43:51 -0500

    .

--- src/sketches/_op_frags_shape_0/data/frag.glsl
+++ src/sketches/_op_frags_shape_0/data/frag.glsl
@@ -5,17 +5,15 @@
 uniform vec2 res;
 
 float circle(vec2 p, float r){
-	vec2 aspectRatio = vec2(res.x / res.y, 1.0);
-	vec2 uv = gl_FragCoord.xy / res * 2.0 - 1.0;
-	return 1.0 - step(r, distance(uv * aspectRatio, p));
+  vec2 aspectRatio = vec2(res.x / res.y, 1.0);
+  vec2 uv = gl_FragCoord.xy / res * 2.0 - 1.0;
+  return 1.0 - step(r, distance(uv * aspectRatio, p));
 }
 
 float ellipse(vec2 p, vec2 r){
-	return 0.0;
+  return 0.0;
 }
 
 void main() {
-  vec3 col = vec3( circle(vec2(0.0), 1.0) );
-  // vec3 col = vec3( ellipse(vec2(0.0), vec2(0.5, 0.5)) );
-  gl_FragColor = vec4(col, 1.0);
+  gl_FragColor = vec4( vec3(circle(vec2(0.0), 1.0)), 1.0 );
 }
